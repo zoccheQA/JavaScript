0557617c3d7b1331adf8da45ef73f9edad34f00a
zoccheQA <zocche.carolineqa@gmail.com>
2025-09-17 15:57:45 -0300

    Ex 14 e 15

---
+++ Class12ex/ex015/script.js
@@ -0,0 +1,11 @@
+function verififcar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano ) {
+        window.alert('Verifique os dados e tente novamente')
+    } else {
+        window.alert('Tudo ok')
+    }
+}
